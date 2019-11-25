cc59e2dffceff7fd2f66dd596169f386b54806c1
Pritesh Gudge <priteshgudge@gmail.com>
2019-11-25 15:53:16 +0530

    Updating logic to enable Order Now Button

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -51,7 +51,7 @@ class BurgerBuilder extends Component {
             .reduce( ( sum, el ) => {
                 return sum + el;
             }, 0 );
-        this.setState( { purchasable: sum > 0 } );
+        return sum > 0
     }
 
     // addIngredientHandler = ( type ) => {
@@ -126,7 +126,7 @@ class BurgerBuilder extends Component {
                         ingredientAdded={this.props.onIngredientAdded}
                         ingredientRemoved={this.props.onIngredientRemoved}
                         disabled={disabledInfo}
-                        purchasable={this.state.purchasable}
+                        purchasable={this.updatePurchaseState(this.props.ings)}
                         ordered={this.purchaseHandler}
                         price={this.props.totPrc} />
                 </Aux>
